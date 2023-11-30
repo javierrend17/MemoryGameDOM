eaf7fa1da5b22c1eef1881bc96ed59a2706278c2
Jonathan Carranza <jhoncarranza1@outlook.es>
2023-11-29 21:49:34 -0600

    creacion de login con estilo y mensajito que dice si se logueó o no

--- memoryGameDOM/main.js
+++ memoryGameDOM/main.js
@@ -1,2 +1,10 @@
-import './style.css'
-
+function validateAndLogin() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    if (username.trim() === '' || password.trim() === '') {
+      alert('Por favor, completa ambos campos.');
+    } else {
+      alert('Inicio de sesión exitoso');
+    }
+  }
